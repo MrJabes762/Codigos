bb6af06765bc64b5781518ba565cb5da3cc0cae9
MrJabes762 <118413268+MrJabes762@users.noreply.github.com>
2022-12-22 13:30:05 -0300

    modified: Pilha_c (#3)

--- Pilha.c
+++ Pilha.c
@@ -2,23 +2,25 @@
 #include <stdlib.h>
 #include <locale.h>
 
-# define MAXTAM 5
+#define MAXTAM 5
 
 typedef int Apontador;
 
-typedef struct {
-  int valor;
+typedef struct
+{
+	int valor;
 } TipoDado;
 
-typedef struct {
-  TipoDado Vdados[MAXTAM];
-  Apontador Topo;
+typedef struct
+{
+	TipoDado Vdados[MAXTAM];
+	Apontador Topo;
 } TipoPilha;
 
-// Operações 
+// Operaï¿½ï¿½es
 void Inicia(TipoPilha *Pilha)
 {
-	Pilha -> Topo = -1;
+	Pilha->Topo = -1;
 }
 
 int Vazia(TipoPilha Pilha)
@@ -28,56 +30,71 @@ int Vazia(TipoPilha Pilha)
 
 void Empilha(TipoDado Dado, TipoPilha *Pilha)
 {
-    if (Pilha->Topo == MAXTAM-1){
-	  printf(" Erro pilha esta  cheia\n");
-	 }
-	else{
-	   Pilha->Topo++;
-	   Pilha->Vdados[Pilha->Topo] = Dado;
-	   }
-}
-
-
-void Desempilha(TipoPilha *Pilha, TipoDado *Dado)
-{
-    if (Vazia(*Pilha)){
-      printf(" Erro   pilha esta  vazia\n");
+	if (Pilha->Topo == MAXTAM - 1)
+	{
+		printf(" Erroï¿½pilha estaï¿½ cheia\n");
 	}
-	else {
-	    *Dado = Pilha->Vdados[Pilha->Topo];
-	    Pilha->Topo--;
-	   }
+	else
+	{
+		Pilha->Topo++;
+		Pilha->Vdados[Pilha->Topo] = Dado;
+	}
 }
 
-void imprimirPilha(TipoPilha Pilha){
-  if(Vazia(Pilha)){
-    printf("\n| Pilha Vazia!");
-  }else{
-    int Aux;
-    for(Aux = Pilha.Topo; Aux >=0; Aux--){	
-      printf("\n[ %dº ] da Pilha: %d\n ",Aux,Pilha.Vdados[Aux].valor);
-    }
-  }
+int Desempilha(TipoPilha *Pilha, TipoDado *Dado)
+{
+	if (Vazia(*Pilha))
+	{
+		printf(" Erroï¿½ï¿½ pilha estaï¿½ vazia\n");
+	}
+	else
+	{
+		*Dado = Pilha->Vdados[Pilha->Topo];
+		Pilha->Topo--;
+	}
+	return Pilha->Vdados[Pilha->Topo + 1].valor;
 }
 
-int main(){
-	TipoPilha p;
-	TipoDado  d;
-	setlocale(LC_ALL,"portuguese");
-	
+void imprimirPilha(TipoPilha Pilha)
+{
+	if (Vazia(Pilha))
+	{
+		printf("\n| Pilha Vazia!");
+	}
+	else
+	{
+		int Aux;
+		for (Aux = Pilha.Topo; Aux >= 0; Aux--)
+		{
+			printf("\n[ %dï¿½ ] da Pilha: %d\n ", Aux, Pilha.Vdados[Aux].valor);
+		}
+	}
+}
+
+int main()
+{
+	int vlr;
+	TipoPilha p, p1;
+	TipoDado d;
+	setlocale(LC_ALL, "portuguese");
 	Inicia(&p);
-	
-	d.valor=1;
-	Empilha(d,&p);
-	d.valor=2;
-	Empilha(d,&p);
-	d.valor=3;
-	Empilha(d,&p);
-	d.valor=4;
-	Empilha(d,&p);
-	
+	d.valor = 1;
+	Empilha(d, &p);
+	d.valor = 2;
+	Empilha(d, &p);
+	d.valor = 3;
+	Empilha(d, &p);
+	d.valor = 4;
+	Empilha(d, &p);
+	d.valor = 5;
+	Empilha (d,&p);
 	imprimirPilha(p);
-	
-	
+	Inicia (&p1);
+		for (int i = MAXTAM - 1; i >= 0; i--){
+			vlr=Desempilha (&p,&d);
+			d.valor=vlr;
+			Empilha (d,&p1);
+		}
+	imprimirPilha (p1);
 	return 0;
 }
