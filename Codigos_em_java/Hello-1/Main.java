de7fed652ef6e58eb870ea9d1df5f32b53471652
MrJabes762 <jabessilva31@gmail.com>
2023-04-15 15:01:10 -0300

    Alterando_arquivos

---
+++ Codigos_em_java/Hello-1/Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+
+    int idade = 15;
+    while (idade <= 18) {
+      System.out.println(idade);
+      idade = idade + 1;
+    }  
+    for (int i = 0; i < 10; i++) {
+      System.out.println("olá!");
+    }
+
+  }
+}
---
+++ Codigos_em_java/exercicio-cap-6/Teste.java
@@ -0,0 +1,10 @@
+
+public class Teste {
+    public static void main (String [] args){
+        Funcionario A = new Funcionario ();
+        A.Nome = "Jabes Silva";
+        A.sal = 1025;
+        System.out.println ("Nome: "+ A.Nome);
+        System.out.println ("Salaio: " +A.sal);
+    }
+}
