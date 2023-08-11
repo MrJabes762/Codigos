74421d14f88cbece6be27db6b7decb1cfb0212c1
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-08-11 15:42:49 -0300

    Exercicio Sistema de Reserva de Eventos

--- Codigos_em_java/Sistema_reserva_de_eventos/SistemaReservadeEventos.java
+++ Codigos_em_java/Sistema_reserva_de_eventos/SistemaReservadeEventos.java
@@ -1,14 +1,20 @@
 public class SistemaReservadeEventos implements OperacoesDoSistema {
+    private BancodeDadosDeEventos banco;
+    private IngressosSelecionados ingressos;
+    
 
+    public SistemaReservadeEventos (){
+        setBanco(new BancodeDadosDeEventos());
+        setIngressos (new IngressosSelecionados());
+    }
     @Override
-    public void cadastrarEventos(Evento evento) {
-       
+    public void cadastrarEventos(Evento evento) {//Feito
+        getBanco().adicionarEventosNoBancoDeDados(evento);
     }
 
     @Override
     public void gerarComprovante() {
-        // TODO Auto-generated method stub
-        
+
     }
 
     @Override
@@ -24,15 +30,25 @@ public class SistemaReservadeEventos implements OperacoesDoSistema {
     }
 
     @Override
-    public List pesquisarEventosdisponiveis() {
-        // TODO Auto-generated method stub
-        return null;
+    public List pesquisarEventosdisponiveis() {//feito
+        return getBanco().getClass().getBancoDeEventos();
     }
 
     @Override
-    public void reservarIngresso(Ingresso ingresso) {
-        // TODO Auto-generated method stub
-        
+    public void reservarIngresso(Ingresso ingresso) {//feito
+        getIngressos().adicinarIngressoSelecionado(ingresso);
+    }
+    public BancodeDadosDeEventos getBanco() {
+        return banco;
+    }
+    public void setBanco(BancodeDadosDeEventos banco) {
+        this.banco = banco;
+    }
+    public IngressosSelecionados getIngressos() {
+        return ingressos;
+    }
+    public void setIngressos(IngressosSelecionados ingressos) {
+        this.ingressos = ingressos;
     }
     
 }
