8128e1d474716b6c1c37a65bdeaeca94e5ef1b91
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-08-14 18:53:56 -0300

    Add codigos (#17)
    * Codigos_recreativos
    
    * Codigos_feitos_em_C++
    
    * Reorganizado_Arquivos
    
    * Reorganizando_arquivos_e_mais_adicoes
    
    * Alterando_arquivos
    
    * Add_Codigos_Java
    
    * Código java Biblioteca
    
    * Exercicio Lanchonete Free Time
    
    * Códigos Amtigos desenvolvidos em estrutura de dado
    
    * modificações
    
    * Novos códigos java
    
    * codigo desnecessario
    
    * Exercicio sistema de reserva de ingresso
    
    * Exercicio Sistema de Reserva de Eventos
    
    * Finalizando exercicio

--- Codigos_em_java/Sistema_reserva_de_eventos/SistemaReservadeEventos.java
+++ Codigos_em_java/Sistema_reserva_de_eventos/SistemaReservadeEventos.java
@@ -1,44 +1,63 @@
+import java.util.Calendar;
+
 public class SistemaReservadeEventos implements OperacoesDoSistema {
 
     private BancodeDadosDeEventos banco;
     private IngressosSelecionados ingressos;
-    
+    private Comprovante comprovante;
+
 
     public SistemaReservadeEventos (){
         setBanco(new BancodeDadosDeEventos());
         setIngressos (new IngressosSelecionados());
     }
+
     @Override
     public void cadastrarEventos(Evento evento) {//Feito
         getBanco().adicionarEventosNoBancoDeDados(evento);
-
-
-    @Override
-    public void cadastrarEventos(Evento evento) {
-       
-
     }
 
     @Override
     public void gerarComprovante() {
-
+        this.comprovante = new Comprovante (getIngressos(),Calendar.getInstance());
     }
 
     @Override
     public Comprovante getComprovante() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.comprovante;
     }
 
     @Override
     public double getTotaldasreservas(Comprovante comprovante) {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.comprovante.getIngressosSelecionado();
     }
 
     @Override
     public List pesquisarEventosdisponiveis() {//feito
         return getBanco().getClass().getBancoDeEventos();
+    }
+
+    @Override
+    public void reservarIngresso(Ingresso ingresso) {//feito
+        getIngressos().adicinarIngressoSelecionado(ingresso);
+    }
+    public BancodeDadosDeEventos getBanco() {
+        return banco;
+    }
+    public void setBanco(BancodeDadosDeEventos banco) {
+        this.banco = banco;
+    }
+    public IngressosSelecionados getIngressos() {
+        return ingressos;
+    }
+    public void setIngressos(IngressosSelecionados ingressos) {
+        this.ingressos = ingressos;
+    }
+    public Calendar getData() {
+        return data;
+    }
+    public void setData(Calendar data) {
+        this.data = data;
     }
 
     @Override
@@ -64,7 +83,7 @@ public class SistemaReservadeEventos implements OperacoesDoSistema {
 
     @Override
     public void reservarIngresso(Ingresso ingresso) {
-
+      
     }
     
 }
