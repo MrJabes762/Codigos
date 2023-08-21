5c442f3c46cc5d797bc3aa63af76eb927668c317
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-08-20 21:17:31 -0300

    Termino da Abstração da Lógica

--- Codigos_em_java/Sistema_reserva_de_quartos_hotel/MainReservaQuartosHotel.java
+++ Codigos_em_java/Sistema_reserva_de_quartos_hotel/MainReservaQuartosHotel.java
@@ -5,7 +5,10 @@ public class MainReservaQuartosHotel {
         sistema.cadastroDeQuartosNoHotel (new Quarto (1,"Solteiro", 30.00, 3));
         sistema.cadastroDeQuartosNoHotel (new Quarto (2,"Casal", 40.00, 4));
         sistema.cadastroDeQuartosNoHotel (new Quarto (3,"Suite", 50.00, 3));
-        sistema.reservarQuartos ();
+        sistema.reservarQuartos (1);//implementar seleção de Datas atual da reserva
+        sistema.reservarQuartos (2);//implementar seleção de Datas atual da reserva
+        System.out.println(sistema.getTotaldasReservas ());
+        sistema.gerarComprovante ();
     }
     /*Uma rede de hotéis está planejando um sistema de reserva de quartos online. 
     O sistema deve permitir que os clientes pesquisem quartos disponíveis, escolham as datas de check-in e check-out, e façam a reserva. 
