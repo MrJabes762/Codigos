528f3fb537663f2d25815ab00333c1b71e3050e5
Mr Jabes <jabessilva31@gmail.com>
2024-08-04 22:19:41 -0300

    Exercicio Biblioteca Musica Digital

---
+++ Codigos_em_java/exercicio_biblioteca_musica_digital/Roteiro.java
@@ -0,0 +1,9 @@
+public class Roteiro {
+    public static void main(String[] args) {
+        
+    }
+}
+/*Biblioteca de Música Digital: Crie um sistema para gerenciar uma biblioteca de música digital.
+ Os usuários podem criar listas de reprodução, cada uma contendo várias músicas. Cada música tem informações, como título, artista e duração. 
+ Os usuários podem adicionar e remover músicas de suas listas de reprodução e reproduzir as músicas em suas listas.
+ Implemente funcionalidades para listar as músicas em uma lista de reprodução, calcular a duração total da lista de reprodução e reproduzir músicas. */
---
+++ Codigos_em_java/exercicio_biblioteca_musica_digital/src/services/BibliotecaMusicaDigitalOperacoes.java
@@ -0,0 +1,5 @@
+public interface BibliotecaMusicaDigitalOperacoes{
+    public abstract void listarMusicaAListaReproducao ();
+    public abstract void adicionarMusicaAListaReproducao (Musica musica);
+    public abstract void removerMusicaListaReproducao (Musica musica);
+}
