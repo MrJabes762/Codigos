bd5958ddd783b751d1dd6ba11b95d6329654e0f5
Mr Jabes <jabessilva31@gmail.com>
2024-08-28 17:11:02 -0300

    Exercicio sistema__gerenciamento_de_escola Java

---
+++ Codigos_em_java/exercicio_sistema_gerenciamento_de_escola/model/Professor.java
@@ -0,0 +1,70 @@
+package model;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import data.Turma;
+
+public class Professor {
+
+    private String nome;
+    private List<Turma> turmas;
+
+    public Professor(String nome, List<Turma> turmas) {
+        setNome(nome);
+        setTurmas((turmas == null) ? new ArrayList<>() : turmas);
+    }
+
+    public Professor(String nome) {
+        this(nome, null);
+    }
+
+    public void removeTurma(Turma turma) {
+        if(turmas.contains(turma)){
+            turmas.add(turma);
+            turma.removeProfessor(this);
+        }
+    }
+
+    public void addTurma(Turma turma) {
+        if(!turmas.contains(turma)){
+            turmas.add(turma);
+            turma.addProfessor(this);
+        }
+    }    
+
+    public List<Turma> getTurmas(){
+        return List.copyOf(turmas);
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Professor professor = (Professor) o;
+        return Objects.equals(nome, professor.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
+    }
+
+
+    public String toString(){
+        return "\n"+getNome();
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTurmas(List<Turma> turmas) {
+        this.turmas = turmas;
+    }
+}
