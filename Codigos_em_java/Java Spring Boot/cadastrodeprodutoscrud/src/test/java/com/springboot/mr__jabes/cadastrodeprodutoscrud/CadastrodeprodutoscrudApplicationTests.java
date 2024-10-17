f68ba8bb7e24910367264437a3bd6257b5f8751e
Mr Jabes <jabessilva31@gmail.com>
2024-10-17 14:02:07 -0300

    Atualizações:
    ** Arquivos de start e test

---
+++ Codigos_em_java/Java Spring Boot/cadastrodeprodutoscrud/src/test/java/com/springboot/mr__jabes/cadastrodeprodutoscrud/CadastrodeprodutoscrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mr__jabes.cadastrodeprodutoscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrodeprodutoscrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
