56ede713c01f3ca3a44760771735784bf289117a
Mr Jabes <jabessilva31@gmail.com>
2024-12-30 11:10:13 -0300

    Atulizações:
    * Exercicios de variaveis e Constantes

---
+++ Codigos_em_JavaScript/Aprendendo/variaveisEConstantes.js
@@ -0,0 +1,10 @@
+var a = 10 // atribuição
+let b = 4 // o let possui regras e formas mais modernas de se declarar variaveis 
+
+var a = 10
+ b = 40 
+
+console.log(a,b)
+
+const c = 5
+console.log (c)// uma constate - imutável 
