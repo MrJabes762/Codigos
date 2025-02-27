ff72d3bc8be0ccf83fd84a7bf7f2609dcc883607
Mr Jabes <jabessilva31@gmail.com>
2025-02-27 12:36:44 -0300

    Atualizações:
    * Adicionar script para filtrar produtos caros e frágeis, e calcular médias de preços

---
+++ Codigos_em_JavaScript/Aprendendo/filter/filter2.js
@@ -0,0 +1,78 @@
+const produtos = [
+    { nome: 'Notebook', preco: 2499, fragil: true },
+    { nome: 'Ipad Pro', preco: 4199, fragil: true },
+    { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+    { nome: 'Copo de Plástico', preco: 18.99, fragil: false },
+    { nome: 'Caneta', preco: 7.50, fragil: false },
+    { nome: 'Caderno', preco: 18.99, fragil: false },
+    { nome: 'Kit de Lápis', preco: 41.22, fragil: true },
+    { nome: 'Borracha', preco: 3.45, fragil: false },
+    { nome: 'Estojo', preco: 12.99, fragil: false },
+    { nome: 'Mochila', preco: 89.99, fragil: false },
+    { nome: 'Lápis', preco: 1.99, fragil: false },
+    { nome: 'Caneta Bic', preco: 2.99, fragil: false },
+    { nome: 'Caneta Pilot', preco: 3.99, fragil: false },
+    { nome: 'Caneta Stabilo', preco: 4.99, fragil: false },
+    { nome: 'Caneta Compactor', preco: 5.99, fragil: false },
+    { nome: 'Caneta Faber Castell', preco: 6.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 7.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 8.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 9.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 10.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 11.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 12.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 13.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 14.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 15.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 16.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 17.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 18.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 19.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 20.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 21.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 22.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 23.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 24.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 25.99, fragil: false },
+    { nome: 'Caneta Crown', preco: 26.99, fragil: false },
+];
+
+// Filtrando os produtos que são caros
+
+const produtosCaros = produto => produto.preco > 15;
+
+// Filtrando os produtos que são frágeis
+
+const produtosNaoFragil = produto => produto.fragil == false;
+
+// Criando uma função para filtrar os produtos que são copos
+
+const produtosCaneta = produto => produto.nome.includes('Caneta');
+
+let produtosCanetaNaoFragilCaros = produtos.filter(produtosCaros).filter(produtosNaoFragil).filter(produtosCaneta);
+
+console.log(produtosCanetaNaoFragilCaros);
+
+// Calculando a média dos preços dos produtos filtrados sem usar reduce
+let valorTotal = 0;
+for (let i = 0; i < produtosCanetaNaoFragilCaros.length; i++) {
+    valorTotal += produtosCanetaNaoFragilCaros[i].preco;
+}
+
+console.log("Valor total dos produtos filtrados:", valorTotal);
+
+let media = valorTotal / produtosCanetaNaoFragilCaros.length;
+
+console.log("Média dos preços dos produtos filtrados:", media);
+
+// Calculando a média dos preços dos produtos sem Reduce e com ForEach
+
+valorTotal = 0;
+
+produtosCanetaNaoFragilCaros.forEach(produto => valorTotal += produto.preco);
+
+console.log("Valor total dos produtos filtrados com ForEach:", valorTotal);
+
+media = valorTotal / produtosCanetaNaoFragilCaros.length;
+
+console.log("Média dos preços dos produtos filtrados com ForEach:", media);
