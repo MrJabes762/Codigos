3cc1ad3a8170ef82328c10affac20df836241c9b
Mr Jabes <jabessilva31@gmail.com>
2025-03-04 07:26:12 -0300

    Atualização:
    *Adição de exemplo sobre a função reduce em JavaScript

---
+++ Codigos_em_JavaScript/Aprendendo/array/reduce/reduce1.js
@@ -0,0 +1,10 @@
+// O reduce é uma função que transforma um array em um unico elemento (pode ser um string, um array, um numero, uma letra...).
+
+// Por parametro, nos teremos acumulador que será chamado de execução em execução somando uma iteração a outra.
+
+// O que será acumulado vai depender do segundo parametro comumente chamado de indice ou valor atual.
+
+// O resultado da primeira iteração é passado para a segunda iteracao e assim por diante até o limite do array.
+
+
+
