2ddd5f874739621d222573ffe6747baeca2ee1be
Mr Jabes <jabessilva31@gmail.com>
2025-03-07 17:20:04 -0300

    Atualizações:
    *Correção de erros de digitação nas mensagens
    
    *ajustes na lógica de envio/recebimento

--- Codigos_em_Java/algoritmosDistribuidos/clocksESincronizacaodeTempo/Main.java
+++ Codigos_em_Java/algoritmosDistribuidos/clocksESincronizacaodeTempo/Main.java
@@ -4,17 +4,20 @@ import service.SistemaDistribuidodeClocksESincronizacaoDetempo;
 public class Main {
     public static void main(String[] args) {
         No1 no1 = new No1("Casa");
-        No1 no2 = new No1 ("Trabalho");
-        No1 no3 = new No1 ("Escritorio");
+        No1 no2 = new No1("Trabalho");
+        No1 no3 = new No1("Escritório");
         SistemaDistribuidodeClocksESincronizacaoDetempo sistema = new SistemaDistribuidodeClocksESincronizacaoDetempo();
-        no1.setMessagem(sistema.criarMensagem("Mensagem Exemplo", "Mensagem Conteudo"));
+        
+        no1.setMensagem(sistema.criarMensagem("Mensagem Exemplo", "Mensagem Conteúdo"));
         no1.enviarMensagem(sistema, no3);
-        no3.receberMensagem(no1.getMessagem());
-        no2.setMessagem(sistema.criarMensagem("Outra Mensagem", "Outro Conteudo"));
+        no3.receberMensagem(sistema, no1.getMensagem());
+        
+        no2.setMensagem(sistema.criarMensagem("Outra Mensagem", "Outro Conteúdo"));
         no2.enviarMensagem(sistema, no1);
-        no1.receberMensagem(no2.getMessagem());
-        no3.setMessagem(sistema.criarMensagem("Mais uma Mensagem", "Mais um Conteudo"));
+        no1.receberMensagem(sistema, no2.getMensagem());
+        
+        no3.setMensagem(sistema.criarMensagem("Mais uma Mensagem", "Mais um Conteúdo"));
         no3.enviarMensagem(sistema, no2);
-        no2.receberMensagem(no3.getMessagem());
+        no2.receberMensagem(sistema, no3.getMensagem());
     }
 }
