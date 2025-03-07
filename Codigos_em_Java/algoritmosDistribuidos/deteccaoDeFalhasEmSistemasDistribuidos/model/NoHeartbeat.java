35978659f25ddff693bb28ee2334569d8121c809
Mr Jabes <jabessilva31@gmail.com>
2025-03-07 18:49:25 -0300

    Atualizações:
    *Atualiza a classe NoHeartbeat para incluir métodos de envio e parada de heartbeat

--- Codigos_em_Java/algoritmosDistribuidos/deteccaoDeFalhasEmSistemasDistribuidos/model/NoHeartbeat.java
+++ Codigos_em_Java/algoritmosDistribuidos/deteccaoDeFalhasEmSistemasDistribuidos/model/NoHeartbeat.java
@@ -1,40 +1,45 @@
 package deteccaoDeFalhasEmSistemasDistribuidos.model;
 
 import algoritmosdeEleicaoBully.model.No;
-import deteccaoDeFalhasEmSistemasDistribuidos.MecanismoDeteccaoDeFalhasHeartbeat;
+import deteccaoDeFalhasEmSistemasDistribuidos.service.MecanismoDeteccaoDeFalhasHeartbeat;
 
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
+//Classe de Representação do NoHeartBeat 
+// extends No do Algoritimo de e Eleicão
 
 public class NoHeartbeat extends No {
     
-    private List<String> notificacoes;
+    //Atributos
     private boolean ativo;
     private Timer timer;
 
+    //Cosntrutor
     public NoHeartbeat(String nomeDispositivo, int i) {
+        // passando os elementos comuns para o construtor do pai 
         super(nomeDispositivo, i);
-        this.ativo = true;
-        this.timer = new Timer();
+        this.ativo = true;// setando o no para ativo 
+        this.timer = new Timer();// estaciando o Timer
     }
 
+    //metodo de envio de heartBeat ao monitor
     public void enviarHeartbeat(MecanismoDeteccaoDeFalhasHeartbeat monitor) {
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                if (ativo) {
+                if (isAtivo()) {// se ativo envia o no heartbeat Autoreferenciado 
                     monitor.receberHeartbeat(NoHeartbeat.this);
                 }
             }
         }, 0, 1000); // Envia heartbeat a cada 1 segundo
     }
-
+    //metodo de parar o envio de heartbeat
     public void pararHeartbeat() {
         this.ativo = false;
     }
-
+    //Metodo de retorno do valor ativo
     public boolean isAtivo() {
         return ativo;
     }
