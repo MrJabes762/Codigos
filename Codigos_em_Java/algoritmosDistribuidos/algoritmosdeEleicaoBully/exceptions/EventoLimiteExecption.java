c03a67d9bcf631df954df61835b8d1f20edd21d9
Mr Jabes <jabessilva31@gmail.com>
2025-03-07 00:46:16 -0300

    Atualizações
    *Adiciona a exceção EventoLimiteExecption para tratamento de erros

---
+++ Codigos_em_Java/algoritmosDistribuidos/algoritmosdeEleicaoBully/exceptions/EventoLimiteExecption.java
@@ -0,0 +1,7 @@
+package algoritmosdeEleicaoBully.exceptions;
+
+public class EventoLimiteExecption extends Exception{
+    public EventoLimiteExecption (String mensagem){
+        super(mensagem);
+    }
+}
