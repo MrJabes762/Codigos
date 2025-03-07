4d84766a7bf740afa8d20a311646e54c4a8e1066
Mr Jabes <jabessilva31@gmail.com>
2025-03-07 14:44:45 -0300

    Atualizações:
    *Atualiza a lógica de exibição e manipulação de nós no algoritmo de eleição Bully

--- Codigos_em_Java/algoritmosDistribuidos/algoritmosdeEleicaoBully/Main.java
+++ Codigos_em_Java/algoritmosDistribuidos/algoritmosdeEleicaoBully/Main.java
@@ -4,29 +4,32 @@ import algoritmosdeEleicaoBully.exceptions.EventoLimiteExecption;
 import algoritmosdeEleicaoBully.model.No;
 
 public class Main {
+
     public static void main(String[] args) {
-        SistemaDeAlgoritmoDeEleicao algoritmo = new SistemaDeAlgoritmoDeEleicao ();
-        System.out.println(algoritmo.adicionarNo(new No("Trabalho", 50)));// o identificador incrementa
-        System.out.println(algoritmo.adicionarNo(new No("Casa",1000)));
-        System.out.println(algoritmo.adicionarNo(new No("Escritorio",100)));
-        System.out.println("Lista de Nos: ");
+        SistemaDeAlgoritmoDeEleicao algoritmo = new SistemaDeAlgoritmoDeEleicao();
+        System.out.println(algoritmo.adicionarNo(new No("Trabalho", 500)));// o identificador incrementa
+        System.out.println(algoritmo.adicionarNo(new No("Casa", 10)));
+        System.out.println(algoritmo.adicionarNo(new No("Escritorio", 100)));
+        System.out.println(algoritmo.setCoordenador());//Ele vai setar o coordenador navegando pela lista e pegar o Identificador mais alto
+        System.out.println(" ------>  Lista de Nos <----");
         System.out.println(algoritmo.getListaDeNos());
-        algoritmo.setCoordenador();//Ele vai setar o coordenador navegando pela lista e pegar o Identificador mais alto
-        System.out.println("No coordenador escolhido com base no Identificador: " + algoritmo.getNoCordenador());
+        System.out.println(" ------>  Lista de Nos <----");
         try {
-            algoritmo.eventoOcorre();//ao setar o coordenador, eventos vao ocorrer
-            algoritmo.eventoOcorre();
-            algoritmo.eventoOcorre();
-            algoritmo.eventoOcorre();// o limitador vai ate 3 eventos ao chegar nesse ele vai estourar a excessão
+            System.out.println(algoritmo.eventoOcorre());//ao setar o coordenador, eventos vao ocorrer
+            System.out.println(algoritmo.eventoOcorre());
+            System.out.println(algoritmo.eventoOcorre());
+            System.out.println(algoritmo.eventoOcorre());// o limitador vai ate 3 eventos ao chegar nesse ele vai estourar a excessão
         } catch (EventoLimiteExecption e) {
             System.out.println(e.getMessage());
-            System.out.println("Lista de Nos: ");
+            System.out.println(algoritmo.setCoordenador());
+            System.out.println(" ------>  Lista de Nos <----");
             System.out.println(algoritmo.getListaDeNos());
-            algoritmo.setCoordenador();
-            System.out.println("No coordenador escolhido com base no Identificador: " + algoritmo.getNoCordenador());
+            System.out.println(" ------>  Lista de Nos <----");
         }
-        //algoritmo.notificacao();
-        //algoritmo.removeCoordenador(); // Simulacao de falha algum coordenador sai do ar ou do sistema isso é a remocao ele deve procurar outro
-        //System.out.println();
+        System.out.println("-> Ouve uma falha no Coordenador " + algoritmo.coordenadorSaiuDoAr().getNome());
+        System.out.println(algoritmo.setCoordenador());
+        System.out.println(" ------>  Lista de Nos <----");
+        System.out.println(algoritmo.getListaDeNos());
+        System.out.println(" ------>  Lista de Nos <----");
     }
 }
