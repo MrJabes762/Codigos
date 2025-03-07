412405edc9be6e540d534fe1fef36aa4ff1b0a01
Mr Jabes <jabessilva31@gmail.com>
2025-03-07 17:58:20 -0300

    Atualizações:
    *Melhora a documentação da classe Processo com comentários explicativos sobre atributos e métodos.

--- Codigos_em_Java/algoritmosDistribuidos/estadoGlobalECapturadeEstado/model/Processo.java
+++ Codigos_em_Java/algoritmosDistribuidos/estadoGlobalECapturadeEstado/model/Processo.java
@@ -3,13 +3,16 @@ package estadoGlobalECapturadeEstado.model;
 import java.util.ArrayList;
 import java.util.List;
 
+// Classe modelo de Representaçao do Processo 
 public class Processo {
+    //Atributos 
     private int id;
-    private boolean gravando;
-    private List<String> estado;
-    private List<String> canalEstado;
-    private List<Processo> vizinhos;
+    private boolean gravando;//Variavel de controle de gravação de estado
+    private List<String> estado;// lista de estado local
+    private List<String> canalEstado;//lista de estado global com foco nas mensagens em tempo de execução
+    private List<Processo> vizinhos;// vizinhos associados a cada processo 
 
+    //Construtor que recebe apenas o id
     public Processo(int id) {
         setId(id);
         setGravando(false);
@@ -17,56 +20,48 @@ public class Processo {
         setCanalEstado(new ArrayList<>());
         setVizinhos(new ArrayList<>());
     }
-
+    //construtor padrão 
     public Processo() {
     }
 
+    //Método de adição de vizinhos a lista 
     public void adicionarVizinho(Processo vizinho) {
         getVizinhos().add(vizinho);
     }
-
+    // Metodo de receber a mensagem e adiconar a lista do canal 
     public String recebendoMensagem(String mensagem) {
         getCanalEstado().add(mensagem);
         return "Processo " + getId() + ": " + getCanalEstado();
     }
-
+    //Metodo de Receber o marcador 
     public void recebeMarcador() {
-        if (!isGravando()) {
+        if (!isGravando()) {//verificação de gravação antes de proceguir 
             setGravando(true);
             if(isGravando()){
-                salvaEstado();
-                enviaMarcador(new ArrayList<>());
+                salvaEstado();//Primeiro a salfa do estado 
+                enviaMarcador(new ArrayList<>());//Depois o envio de marcadores 
             }
         }
     }
-
+    // Metodo de Salva de estado 
     private void salvaEstado() {
         getEstado().add("Estado do Processo: " + toString());
     }
-
+    //Metodo de envio de marcadores 
     private void enviaMarcador(List<Integer> visitados) {
         visitados.add(getId());
-        for (Processo vizinho : getVizinhos()) {
-            if (!visitados.contains(vizinho.getId())) {
-                vizinho.recebeMarcador();
+        for (Processo vizinho : getVizinhos()) {// verificando se dentre a lista de visinhos 
+            if (!visitados.contains(vizinho.getId())) {// não tiver algum que não esteja visitado 
+                vizinho.recebeMarcador();//ele recebe o marcador 
             }
         }
     }
-
+    //Metodo de printar o estado 
     public String printEstado() {
         return "Estado do processo: " + String.join(", ", getEstado()) + " Estado do Canal do Processo: " + String.join(", ", getCanalEstado());
     }
 
-    @Override
-    public String toString() {
-        return "{" +
-            " id='" + getId() + "'" +
-            ", gravando='" + isGravando() + "'" +
-            ", canalEstado='" + getCanalEstado() + "'" +
-            ", vizinhos='" + getVizinhos().stream().map(Processo::getId).toList() + "'" +
-            "}";
-    }
-
+    //Metodos de acesso
     public int getId() {
         return this.id;
     }
@@ -105,5 +100,15 @@ public class Processo {
 
     public void setVizinhos(List<Processo> vizinhos) {
         this.vizinhos = vizinhos;
+    }
+    //Metodo utilizao pelo anterior para retornar o estado dos atributos
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", gravando='" + isGravando() + "'" +
+            ", canalEstado='" + getCanalEstado() + "'" +
+            ", vizinhos='" + getVizinhos().stream().map(Processo::getId).toList() + "'" +
+            "}";
     }
 }
