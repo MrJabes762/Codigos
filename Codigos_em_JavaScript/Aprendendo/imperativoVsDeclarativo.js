4663fe3a73a09d6249788657ebb0303190c77b1f
Mr Jabes <jabessilva31@gmail.com>
2025-03-12 16:58:55 -0300

    Atualizações:
    * Adiciona exemplo de comparação entre programação imperativa e declarativa em JavaScript

---
+++ Codigos_em_JavaScript/Aprendendo/imperativoVsDeclarativo.js
@@ -0,0 +1,16 @@
+import { alunos } from "./array/reduce/reduce1";
+
+// Imperativo - Menos reutilização de código, realiza mais passos: busca da nota e reliza atribuicao + soma
+let total1 = 0;
+for (let i = 0; i < alunos.length; i++){
+    total1 += alunos[i].nota;
+}
+console.log(total1/alunos.length);
+
+// Declarativo - Mais reutilização de código, cada função pode ser reutilizada em outros contextos
+const getNota = aluno => aluno.nota;
+const soma = (total, atual) => total + atual;
+
+const total2 = alunos.map(getNota).reduce(soma);
+console.log(total2/alunos.length);
+
