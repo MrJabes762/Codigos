2d53b9e57e533feab7d0c0242014c5a277c30c8e
Mr Jabes <jabessilva31@gmail.com>
2025-03-21 20:51:21 -0300

    Atualizações:
    *Adiciona Componente1 ao App.
    
    * Inclusão do comentário sobre a importação do React

--- Codigos_em_ReactNative/JavaScript/exercicios/App.jsx
+++ Codigos_em_ReactNative/JavaScript/exercicios/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from 'react';// sempre que se for utilizar JSX é necessário importar o React
 // O safeAreaView é um componente que garante que o conteúdo da aplicação não fique escondido por baixo da barra de status ou notificações do dispositivo.
 import { Text, SafeAreaView} from 'react-native';
 import { stylesApp } from './styleApp';
+import Componente1 from './src/components/componente1/Componente1';
 
 
 export default function App() {
   return (
     <SafeAreaView style={stylesApp.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Componente1/>
     </SafeAreaView>
   );
 }
