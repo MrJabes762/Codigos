5eb4c2ba2410c61cae6b59023e971220d270472f
Mr Jabes <jabessilva31@gmail.com>
2025-03-21 20:56:52 -0300

    Aatulizações:
    * Adiciona estilo ao texto do Componente1

--- Codigos_em_ReactNative/JavaScript/exercicios/src/components/componente1/Componente1.jsx
+++ Codigos_em_ReactNative/JavaScript/exercicios/src/components/componente1/Componente1.jsx
@@ -1,8 +1,9 @@
 import React from "react";// sempre que se for utilizar JSX é necessário importar o React
 import {Text} from "react-native";
+import { stylesComponente1 } from "./styleComponente1";
 
 export default function Componente1() {
     return (
-        <Text>Primeiro Componente </Text>
+        <Text style = {stylesComponente1.text}>Primeiro Componente </Text>
     );
 }
