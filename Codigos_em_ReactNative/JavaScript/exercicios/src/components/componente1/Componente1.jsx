08f263998157f68844d79b4e69e3dd2e310fae37
Mr Jabes <jabessilva31@gmail.com>
2025-03-21 20:56:11 -0300

    Atualizações:
    *Adicionar Componente1 básico em React Native

---
+++ Codigos_em_ReactNative/JavaScript/exercicios/src/components/componente1/Componente1.jsx
@@ -0,0 +1,8 @@
+import React from "react";// sempre que se for utilizar JSX é necessário importar o React
+import {Text} from "react-native";
+
+export default function Componente1() {
+    return (
+        <Text>Primeiro Componente </Text>
+    );
+}
