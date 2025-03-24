5ac678c6cd671c2e9d2b4a9ad203d87803dd0676
Mr Jabes <jabessilva31@gmail.com>
2025-03-24 19:42:33 -0300

    Atualizações:
    *Adicionar novo componente Multi com múltiplas exportações

---
+++ Codigos_em_ReactNative/JavaScript/exercicios/src/components/Multi/Multi.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text } from "react-native";
+
+//Exportando varios componentes
+//O export funciona semelhante ao emcapulamento
+// Ja  Export defuolt exporta o componente principal (padrao)
+//O export default pode ser importado com qualquer nome
+
+
+export default function Comp() {
+    return (
+        <Text>Terceiro #Oficial</Text>
+    );
+};
+
+export function Comp4() {
+    return (
+        <Text>Quarto Componente</Text>
+    );
+};
+
+export function Comp5() {
+    return (
+        <Text>Quinto Componente</Text>
+    );
+};
