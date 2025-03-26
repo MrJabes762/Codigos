82bd0d99464e9be6e91261d7aa60483d7d3aa5f8
Mr Jabes <jabessilva31@gmail.com>
2025-03-26 12:46:17 -0300

    Atualizações:
    *Adicionar aviso no console para visualização de mensagens no Componente1

--- Codigos_em_ReactNative/JavaScript/exercicios/src/components/componente1/Componente1.jsx
+++ Codigos_em_ReactNative/JavaScript/exercicios/src/components/componente1/Componente1.jsx
@@ -3,6 +3,7 @@ import {Text} from "react-native";
 import { stylesComponente1 } from "./styleComponente1";
 
 export default function Componente1() {
+    console.warn('Comp principal');// artificio utilizafo para visualização de mensagens no console
     return (
         <Text style = {stylesComponente1.text}>Primeiro Componente </Text>
     );
