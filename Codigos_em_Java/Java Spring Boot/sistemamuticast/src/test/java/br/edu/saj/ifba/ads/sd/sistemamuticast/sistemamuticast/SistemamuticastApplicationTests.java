8f6d4f15de42a669f2fe5e399bd129b685ee1fb6
Mr Jabes <jabessilva31@gmail.com>
2025-04-02 09:26:38 -0300

    Atualizações:
    * Adicionar testes de contexto para a aplicação Sistemamuticast

---
+++ Codigos_em_Java/Java Spring Boot/sistemamuticast/src/test/java/br/edu/saj/ifba/ads/sd/sistemamuticast/sistemamuticast/SistemamuticastApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.saj.ifba.ads.sd.sistemamuticast.sistemamuticast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemamuticastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
