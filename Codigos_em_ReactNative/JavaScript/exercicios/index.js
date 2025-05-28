73fdb809aee9ac234dba1572443957bcc41fa48f
Mr Jabes <jabessilva31@gmail.com>
2025-05-28 16:59:43 -0300

    Remove linha em branco e ajusta comentário sobre importação do metro-runtime

--- Codigos_em_ReactNative/JavaScript/exercicios/index.js
+++ Codigos_em_ReactNative/JavaScript/exercicios/index.js
@@ -1,6 +1,6 @@
+import '@expo/metro-runtime'; // Essa importação ajuda no processo de Hotreloading para visualizar uma alteração em tempo real 
 import { registerRootComponent } from 'expo';
 import App from './App';
-
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
