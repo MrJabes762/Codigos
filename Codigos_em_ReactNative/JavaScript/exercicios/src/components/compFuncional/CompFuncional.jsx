6e3b9c0f38b45c9c55ef61c0c8c1dc5ae6c646e3
Mr Jabes <jabessilva31@gmail.com>
2025-05-29 16:25:53 -0300

    feat: Adiciona componente funcional básico em React Native

---
+++ Codigos_em_ReactNative/JavaScript/exercicios/src/components/compFuncional/CompFuncional.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import estiloPadrao from '../Multi/estiloPadrao';
+
+export default props => {
+    return (
+       <Text style = {estiloPadrao.textoGrande}></Text>
+);
+}
