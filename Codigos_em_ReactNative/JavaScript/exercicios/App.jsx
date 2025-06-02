e234ec8f8815051af50cf0d2054e1b61a4637b5c
Mr Jabes <jabessilva31@gmail.com>
2025-06-02 20:02:46 -0300

    feat: Adiciona componentes Pai e PaiInd para comunicação direta e indireta

--- Codigos_em_ReactNative/JavaScript/exercicios/App.jsx
+++ Codigos_em_ReactNative/JavaScript/exercicios/App.jsx
@@ -10,32 +10,44 @@ import Aleatorio from './src/components/aleatorio /Aleatorio';
 import Titulo from './src/components/frag/Titulo';
 import Botao from './src/components/botao/Botao';
 import Contador from './src/components/contador/Contador';
+import Pai from './src/components/direta/Pai';
+import PaiInd from './src/components/indireta/PaiInd';
 
 
 export default function App() {
   return (
     <SafeAreaView style={stylesApp.container}>
+      {/*Componentes Iniciais o Primeiro é o Padrão e o Segundo cita a fragmentação*/}
       <PrimeiroComponente/>
       <SegundoComponente/>
+      {/*Componentes Multi*/}
       <CompPadrao/>
       <Comp4/>
       <Comp5/>
+      {/*Exibindo Valores Desestruturados*/}
       <MinMax min= {4} max = {3}/>
+      {/*Numeros Aleatorios com Desestruturação*/}
       <Aleatorio min = {1} max = {60}/>
       <Aleatorio min = {1} max = {40}/>
+      {/*Exemplos Basicos de Botão*/}
+      <Botao
+      />
+      {/*Propriedades de Leitura*/}
       <Titulo 
         principal = "Ola esse é o texto"
         secundario = "Esse é o texto secundário"
       />
-      <Botao
-      />
       {/* Contador Com Propriedades*/}
-      <Contador
+      {/*<Contador
         iniciar = {10}
         passo = {2}
-      />
+      />*/}
       {/* Contador Sem Propriedades*/}
       <Contador/>
+      {/*Comunicação Direta */}
+      <Pai x = {10} y = {20}/>
+      {/* Comunicação Indireta */}
+      <PaiInd inicial = {10} final = {20}/>
     </SafeAreaView>
   );
 }
