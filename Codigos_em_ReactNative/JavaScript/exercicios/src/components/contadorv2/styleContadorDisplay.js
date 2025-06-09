98ac0c095c4421e3e09bf5f8e3e355441d40a4a4
Mr Jabes <jabessilva31@gmail.com>
2025-06-09 19:15:23 -0300

    feat: Adiciona componente ContadorDisplay e estilos correspondentes

---
+++ Codigos_em_ReactNative/JavaScript/exercicios/src/components/contadorv2/styleContadorDisplay.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const stylesContadorDisplay = StyleSheet.create ({
+    Display:{
+        backgroundColor: 'blue',
+        width: '80%',
+        height: '40%',
+        borderColor: 'black',
+        borderWidth: 3,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    DisplayTexto: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+    }
+});
