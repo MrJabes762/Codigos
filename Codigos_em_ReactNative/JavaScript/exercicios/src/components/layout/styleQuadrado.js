1e01e7b709b052fd168a6e33cfbd52712d01f63c
Mr Jabes <jabessilva31@gmail.com>
2025-07-15 12:43:27 -0300

    feat: Adiciona componente Quadrado e seu estilo para exibição

---
+++ Codigos_em_ReactNative/JavaScript/exercicios/src/components/layout/styleQuadrado.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const lado = 50;
+
+export const styleQuadrado = StyleSheet.create ({
+    quadrado:{
+        height:lado,
+        width:lado,
+    }
+});
