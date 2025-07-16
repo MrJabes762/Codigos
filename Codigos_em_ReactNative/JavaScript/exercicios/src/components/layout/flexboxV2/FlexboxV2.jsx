2cc16f205c09a9f3e834d469349494fa8a311dd9
Mr Jabes <jabessilva31@gmail.com>
2025-07-16 18:11:08 -0300

    feat: Adiciona componente FlexboxV2 e sua estilização

---
+++ Codigos_em_ReactNative/JavaScript/exercicios/src/components/layout/flexboxV2/FlexboxV2.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { stylesFlexboxV2 } from "./styleFlexboxV2";
+import Quadrado from "../Quadrado";
+
+export default (props) => {
+  return (
+    <View style={stylesFlexboxV2.container}>
+      <Quadrado />
+      <Quadrado cor={"red"} />
+      <Quadrado cor={"#0F0"} />
+      <Quadrado cor={"#00F"} />
+      <Quadrado cor={"#009"} />
+      <Quadrado cor={"#900"} />
+      <Quadrado cor={"#090"} />
+    </View>
+  );
+};
